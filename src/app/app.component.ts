96103f9be89f8656bc4d02782cd39f6732512898
Rahul.Yadav <Rahul.Yadav@MLBBT-12447>
2018-06-18 12:35:42 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
-}
+ import {Component, OnInit} from "@angular/core";
+ import {ApiService} from "./api.service";
+ import {IPosts} from "./posts";
+ 
+ @Component({
+     selector: 'app-root',
+     templateUrl: './app.component.html',
+     styleUrls: ['./app.component.css'],
+     providers: [ApiService]
+ })
+ export class AppComponent implements OnInit {
+     _postsArray: IPosts[];
+ 
+     constructor(private apiSerivce: ApiService) {
+     }
+ 
+     getPosts(): void {
+         this.apiSerivce.getPosts()
+             .subscribe(
+                 resultArray => this._postsArray = resultArray,
+                 error => console.log("Error :: " + error)
+             )
+     }
+ 
+     ngOnInit(): void {
+         this.getPosts();
+     }
+ }
